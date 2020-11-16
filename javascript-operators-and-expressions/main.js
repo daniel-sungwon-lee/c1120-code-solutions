7ae371dc7f3858ee31892412d075e9424875eee6
Daniel Sungwon Lee <danlee333@gmail.com>
2020-11-16 15:20:07 -0800

    completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+var width = 3
+var height = 7
+var area = width * height
+  console.log("value of area:", area)
+  console.log("type of area:",typeof area)
+
+var bill = 33
+var payment = 333
+var change = payment - bill
+  console.log("value of change:",change)
+  console.log("type of change:",typeof change)
+
+var quizzes = 98
+var midterm = 96
+var final = 93
+var grade = (quizzes + midterm + final) / 3
+  console.log("value of grade:",grade)
+  console.log("type of grade:",typeof grade)
+
+var firstName = "Daniel"
+var lastName = "Lee"
+var fullName = firstName + " " + lastName
+  console.log("value of fullName:",fullName)
+  console.log("type of fullName:",typeof fullName)
+
+var pH = 3.3
+var isAcidic = pH < 7
+  console.log("value of isAcidic:",isAcidic)
+  console.log("type of isAcidic:",typeof isAcidic)
+
+var headCount = 19
+var isSparta = headCount === 300
+  console.log("value of isSparta:",isSparta)
+  console.log("type of isSparta:",typeof isSparta)
+
+var motto = fullName
+var motto = motto + " is the GOAT"
+  console.log("value of motto:",motto)
+  console.log("type of motto:",typeof motto)
