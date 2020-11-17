4fdd80f771ba3a9732481e726e2b6e2e6014010c
Daniel Sungwon Lee <danlee333@gmail.com>
2020-11-17 10:46:44 -0800

    completed javascript-data-model exercise.

---
+++ javascript-data-model/main.js
@@ -0,0 +1,33 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderNumber: "114-3941689-8772232",
+    datePlaced: "08/04/20",
+    numberOfItems: 1,
+    item1Name: "Javascript for impatient programmers",
+    orderTotal: "$34"
+  },
+  {
+    orderNumber: "113-9984268-1280257",
+    datePlaced: "07/19/20",
+    numberOfItems: 1,
+    item1Name: "The Timeless Way of Building",
+    orderTotal: "$44.53"
+  },
+  {
+    orderNumber: "114-2875557-9059409",
+    datePlaced: "07/04/20",
+    numberOfItems: 1,
+    item1Name: "Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII UIEvent, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter",
+    orderTotal: "$17.22"
+  },
+  {
+    orderNumber: "113-2883177-2648248",
+    datePlaced: "07/03/20",
+    numberOfItems: 2,
+    item1Name: "GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)",
+    item2Name: "The Art of Sql",
+    orderTotal: "$138.93"
+  }
+];
