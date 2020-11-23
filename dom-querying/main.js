7190e71d50cff6676518b76ddb49c3fe84d1be48
Daniel Sungwon Lee <danlee333@gmail.com>
2020-11-23 10:39:50 -0800

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello,world');
+console.log("h1 element:",document.querySelector("h1"));
+console.dir(document.querySelector("h1"))
+console.log("#explanation:",document.querySelector("#explanation"))
+console.dir(document.querySelector("#explanation"))
+console.log(".hint:",document.querySelector(".hint"))
+console.dir(document.querySelector(".hint"))
+console.log("p NodeList:",document.querySelectorAll("p"))
+console.log(".example-link NodeList:",document.querySelectorAll(".example-link"))
