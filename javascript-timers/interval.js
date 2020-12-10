33ea8a722f73d0f28484bbf4fc8032d40595b9a4
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-09 17:52:32 -0800

    minor tweak made to interval.js (clearInterval)

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,11 +1,12 @@
 var $h1=document.querySelector("h1")
 
-setInterval(function(number){
+var countDownInt=setInterval(function(number){
   number=$h1.textContent
   if (number>1){
     number--
     $h1.textContent=number
   }else{
     $h1.textContent="~Earth Beeeelooowww Us~"
+    clearInterval(countDownInt)
   }
 },1000)
