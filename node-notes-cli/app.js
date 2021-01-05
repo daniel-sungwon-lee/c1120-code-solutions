9cbc1c3a70d330948b22783c07c7d623508717a9
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-05 11:44:02 -0800

    completed step 2 add note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -11,7 +11,14 @@ switch(keyword){
     }
     break
   case "create":
-
+    notes[nextId]=process.argv[3]
+    data.nextId=++nextId
+    fs.writeFile("data.json", JSON.stringify(data,null,2),(err)=>{
+      if (err){
+        console.error(err)
+        process.exit(1)
+      }
+    })
     break
   case "update":
     break
