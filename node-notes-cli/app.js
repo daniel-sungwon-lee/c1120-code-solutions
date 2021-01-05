ce2dac034ac9d6996a371b21c2f414e1defaabf6
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-05 11:10:55 -0800

    completed step 1 read notes

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,1 +1,20 @@
 const fs = require("fs")
+const data = require("./data.json")
+const keyword = process.argv[2]
+
+let {nextId,notes}=data
+
+switch(keyword){
+  case "read":
+    for (const[key,value] of Object.entries(notes)){
+      console.log(`${key}: ${value}`)
+    }
+    break
+  case "create":
+
+    break
+  case "update":
+    break
+  case "delete":
+    break
+}
