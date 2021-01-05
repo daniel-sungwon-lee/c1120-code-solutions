7153a752ae3a6d919cc3f0742dfa7af46c8964d8
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-04 17:31:03 -0800

    edited files (added \n)

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-fs.writeFile("note.txt", process.argv[2], (err)=>{
+fs.writeFile("note.txt", process.argv[2] + "\n", (err)=>{
   if (err){
     console.error(err)
     process.exit(1)
--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 let data=Math.random()
 
-fs.writeFile("random.txt",data,(err)=>{
+fs.writeFile("random.txt",data + "\n",(err)=>{
   if (err){
     console.error(err)
     process.exit(1)
