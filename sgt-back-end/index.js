5df0547932b01e60f3a93053ed98afacf36bee7f
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-13 19:15:05 -0800

    added put feature

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -27,10 +27,13 @@ app.get("/api/grades",(req,res)=>{
 
 
 app.post("/api/grades", (req, res) => {
-  if ("name" in req.body && "course" in req.body && "score" in req.body && Number.isInteger(parseFloat(req.body.score)) && req.body.score>0 && Object.keys(req.body).length===3){
+  if ("name" in req.body && "course" in req.body && "score" in req.body &&
+  isNaN(req.body.score) === false && Number.isInteger(parseFloat(req.body.score)) &&
+  req.body.score>0 && Object.keys(req.body).length===3){
+
     const sql=`
       insert into "grades" ("name","course","score")
-      values ('${req.body.name}', '${req.body.course}', '${req.body.score}')
+      values ('${req.body.name}', '${req.body.course}', ${parseInt(req.body.score)})
       returning *
     `
 
@@ -48,6 +51,49 @@ app.post("/api/grades", (req, res) => {
     res.status(400).json({error: "Please check inputs"})
   }
 })
+
+
+app.put("/api/grades/:gradeId",(req,res)=>{
+  if ("name" in req.body && "course" in req.body && "score" in req.body &&
+  isNaN(req.body.score) === false && Number.isInteger(parseFloat(req.body.score)) &&
+  req.body.score > 0 && Object.keys(req.body).length === 3 && isNaN(req.params.gradeId) === false &&
+  req.params.gradeId > 0 && Number.isInteger(parseFloat(req.params.gradeId))){
+
+    const gradeId = parseInt(req.params.gradeId)
+
+    const sql = `
+      update "grades"
+      set "name"='${req.body.name}',
+          "course"='${req.body.course}',
+          "score"='${req.body.score}'
+      where "gradeId"= $1
+      returning *
+    `
+
+    const value = [gradeId]
+
+    db.query(sql,value)
+      .then(result=>{
+        const grade = result.rows[0]
+
+        if(!grade){
+          res.status(404).json({error: `Cannot find grade with gradeId ${gradeId}`})
+
+        } else{
+          res.status(200).json(grade)
+        }
+      })
+
+      .catch(err=>{
+        res.status(500).json({error: "An unexpected error occurred."})
+      })
+
+  } else{
+    res.status(400).json({error: "Please check inputs"})
+  }
+})
+
+
 
 
 
