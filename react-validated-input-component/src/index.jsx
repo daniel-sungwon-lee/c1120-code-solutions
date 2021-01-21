6384df89dfc1163d7b516109c07c19a5cf8dbf80
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-20 17:58:27 -0800

    completed react-validated-input-component challenge

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -1,2 +1,56 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
+class ValidatedInput extends React.Component{
+  constructor(props){
+    super(props)
+    this.handleChange=this.handleChange.bind(this)
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.state={password:"", pass:false, message:"A password is required"}
+  }
+
+  handleChange(event){
+    this.setState({password: event.target.value})
+    if(event.target.value.length<8){
+      this.setState({pass:false, message:"Your password is too short"})
+    } else if(event.target.value.length>=8){
+      this.setState({pass:true, message:""})
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    console.log("password:",this.state.password)
+  }
+
+  render(){
+    const pass = this.state.pass
+    if (!pass){
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <label for="password">Password</label>
+          <div className="text">
+            <input onChange={this.handleChange} type="password" id="password" value={this.state.password} minlength="8" required/>
+            <i class="fas fa-times"></i>
+          </div>
+          <p>{this.state.message}</p>
+        </form>
+      )
+    } else if(pass){
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <label for="password">Password</label>
+          <div className="text">
+            <input onChange={this.handleChange} type="password" id="password" value={this.state.password} minlength="8" required/>
+            <i class="fas fa-check"></i>
+          </div>
+          <p>{this.state.message}</p>
+        </form>
+      )
+    }
+  }
+}
+
+ReactDOM.render(
+  <ValidatedInput />,document.querySelector("#root")
+)
