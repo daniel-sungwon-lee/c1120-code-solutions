0e6922502e8f08df380b5342719e3194403bce6e
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-22 10:54:21 -0800

    refactored code

--- express-static/index.js
+++ express-static/index.js
@@ -5,7 +5,9 @@ const path = require("path")
 
 const absPath=path.join(__dirname,"public")
 
-const files=app.use(express.static("public"))
+const files = express.static(absPath)
+
+app.use(files)
 
 app.listen(3000,()=>{
   console.log("Listening on port 3000")
