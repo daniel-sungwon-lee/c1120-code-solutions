6bed58056cb9b34042dc117e4a0b7ba2d7d4b0a6
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-26 17:30:22 -0800

    WIP carousel

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,11 +3,24 @@ import React from "react"
 class Carousel extends React.Component{
   constructor(props){
     super(props)
-    this.state={}
+    this.state={imageId:null}
+    this.handleClick=this.handleClick.bind(this)
+  }
+
+  handleClick(shownImageId){
+
   }
 
   render(){
-
+    return (
+      <div className="container">
+        <div className="row">
+          <div>
+            <img>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
