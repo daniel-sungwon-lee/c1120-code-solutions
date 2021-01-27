a1f94ef88a31207a62b99d677e3ed9ecb3935f16
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-26 17:15:34 -0800

    added react-carousel-component

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Carousel extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={}
+  }
+
+  render(){
+
+  }
+}
+
+export default Carousel
