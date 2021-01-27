7aa005ee84f92f6e64a5cb0460b17a36b0ff8d12
Daniel Sungwon Lee <danlee333@gmail.com>
2021-01-26 22:58:00 -0800

    completed react-carousel-component challenge

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,12 +3,41 @@ import React from "react"
 class Carousel extends React.Component{
   constructor(props){
     super(props)
-    this.state={imageId:null}
-    this.handleClick=this.handleClick.bind(this)
+    this.state={imageId:1}
+
+    this.handleRightClick=this.handleRightClick.bind(this)
+    this.handleLeftClick=this.handleLeftClick.bind(this)
+    this.handleDotClick=this.handleDotClick.bind(this)
   }
 
-  handleClick(shownImageId){
+  componentDidMount(){
+    setInterval(()=>{
+      if (this.state.imageId<this.props.images.length){
+        this.setState({imageId:this.state.imageId+1})
+      }else{
+        this.setState({imageId:1})
+      }
+    },3000)
+  }
 
+  handleRightClick(id){
+    if (id<this.props.images.length){
+      this.setState({imageId:id +1})
+    }else {
+      this.setState({imageId:1})
+    }
+  }
+
+  handleLeftClick(id){
+    if (id===1){
+      this.setState({imageId:this.props.images.length})
+    }else {
+      this.setState({imageId:id-1})
+    }
+  }
+
+  handleDotClick(id){
+    this.setState({imageId:id})
   }
 
   render(){
@@ -16,19 +45,34 @@ class Carousel extends React.Component{
       <div className="container">
         {
           this.props.images.map(image=>{
+            let rowClass="row hidden"
 
+            if (this.state.imageId===image.id){
+              rowClass="row"
+            }
             return (
-              <div className="row">
+              <div className={rowClass}>
                 <div>
                   <img src={image.path} alt="pokemon"/>
                 </div>
                 <div className="dots">
-                  <i class="far fa-circle"></i>
+                  {
+                    this.props.images.map(image=>{
+                      let dotClass="far fa-circle"
+
+                      if (this.state.imageId===image.id){
+                        dotClass="fas fa-circle"
+                      }
+                      return (
+                        <i onClick={()=>this.handleDotClick(image.id)} class={dotClass}></i>
+                      )
+                    })
+                  }
                 </div>
-                <div className="left-arrow">
+                <div onClick={()=>this.handleLeftClick(image.id)} className="left-arrow">
                   <i class="fas fa-angle-left"></i>
                 </div>
-                <div className="right-arrow">
+                <div onClick={()=>this.handleRightClick(image.id)} className="right-arrow">
                   <i class="fas fa-angle-right"></i>
                 </div>
               </div>
